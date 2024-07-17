d092cb0d51da11f4be89690ce15f21b9fd9ae414
RamazanAbdurahmanov <r0ma.abdurahmanov93@gmail.com>
2024-07-18 00:22:05 +0400

    init commit

---
+++ config-server/src/test/java/az/ramazan/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ramazan.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
